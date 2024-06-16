6a4502b8af642c53bd011fbe204a1dc8781e3150
MattSafi <matthewsafi798@gmail.com>
2024-06-16 20:43:12 +0200

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/BlackJack/' : './',
+  base: '/dist/',
   plugins: [
     vue(),
     vueJsx(),
